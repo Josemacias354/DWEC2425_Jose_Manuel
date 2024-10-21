108a0927e5cdad309a8627c40883d096f075e733
Josemacias354 <maciasbichillo1@gmail.com>
2024-10-21 09:48:27 +0200

    update

--- Unidad3/u3e2_domingos.js
+++ Unidad3/u3e2_domingos.js
@@ -3,10 +3,7 @@ const month = 10; // Noviembre
 const day = 1; // Día 1
 
 for (let year = new Date().getFullYear(); year <= 2100; year++) {
-    let birthday = new Date(year, month - 1, day);
-    if (birthday.getDay() === 0) { // 0 means Sunday
-        console.log(`Your birthday falls on a Sunday in the year ${year}`);
-    }
+
 }
 
 
