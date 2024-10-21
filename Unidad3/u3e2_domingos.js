04d4d766dd27c62c977ffedb0e56d4a7620f3558
Josemacias354 <148208997+Josemacias354@users.noreply.github.com>
2024-10-21 09:46:54 +0200

    Update u3e2_domingos.js

--- Unidad3/u3e2_domingos.js
+++ Unidad3/u3e2_domingos.js
@@ -3,10 +3,7 @@ const month = 5; // May (0-based index, so 5 means June)
 const day = 24; // 24th day
 
 for (let year = new Date().getFullYear(); year <= 2100; year++) {
-    let birthday = new Date(year, month - 1, day);
-    if (birthday.getDay() === 0) { // 0 means Sunday
-        console.log(`Your birthday falls on a Sunday in the year ${year}`);
-    }
+
 }
 
 
