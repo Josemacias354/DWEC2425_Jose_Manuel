540d1913fcaf0299557c09e6540ac9d79e3a5cef
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-06 10:14:48 +0100

    q

--- Prueba Js/script.js
+++ Prueba Js/script.js
@@ -1,10 +1,8 @@
-function confirmClose() {
-    alert("Error 107x. Un virus ha infestado tu disco duro,tenemos que eliminar todos los archivos i formatear tu unidad C:.")
-    if (confirm("Informe al vendedor de este error")) {
-        alert("El navegador se cerrará debido a que es imposible comunicarse con ningun sitio web .");
-    }
-    else {
-        alert("Es impossible recuperar el sistema , se comenzara a eliminar el archivo.");
-        parent.close();
-    }
+
+var texto = prompt("Introduce una cadena de texto");
+
+function eliminarnpj(texto) {
+    let textoSinVocales = texto.replace(/[npj]/gi, "");
+    return textoSinVocales;
 }
+alert(eliminarnpj(texto));
